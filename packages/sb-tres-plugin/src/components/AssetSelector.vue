f05f50c4453d5bc70d35d9bbe8046b061ce26228
alvarosabu <alvaro.saburido@gmail.com>
2023-03-29 09:22:21 +0200

    chore: initial commit

---
+++ packages/sb-tres-plugin/src/components/AssetSelector.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+import { PluginActions, PluginState } from '@storyblok/field-plugin'
+
+const props = defineProps<{
+  selectAsset: PluginActions['selectAsset']
+  data: PluginState
+}>()
+
+const imageUrl = ref('')
+
+const handleSelectAsset = async () => {
+  imageUrl.value = await props.selectAsset()
+}
+</script>
+
+<template>
+  <div className="asset-selector">
+    <button
+      class="btn"
+      @click="handleSelectAsset"
+    >
+      Select Asset
+    </button>
+    <span>Image Url: {{ imageUrl }}</span>
+  </div>
+</template>
